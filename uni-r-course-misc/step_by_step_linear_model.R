a061c3aace5e7117ec394e62a2b9e6a2bfe5a252
Lauri Kajakko <kajakko.lauri@gmail.com>
2022-05-31 21:27:22 +0300

    Refactor

--- uni-r-course-misc/step_by_step_linear_model.R
+++ uni-r-course-misc/step_by_step_linear_model.R
@@ -1,57 +1,43 @@
 #!/usr/bin/env Rscript
 
-# Parse command line arguments
+# Utility functions
+power_two <- function(x) return(x^2)
 
+# Parse command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 vargs <- strsplit(args, ",")
 xs <- as.numeric(vargs[[1]])
 ys <- as.numeric(vargs[[2]])
 
 # Calculate means
-
 x_mean <- mean(xs)
 y_mean <- mean(ys)
 
 # Size of data
-
 n <- length(xs)
 
-S_xx <- sum(
-  sapply(
-    xs,
-    function(x) x^2
-  )
-) - n * x_mean^2
-
-S_xy <- sum(
-  mapply(
-    function(x, y) x * y,
-    xs,
-    ys
-  )
-) - n * x_mean * y_mean
-
-S_yy <- sum(
-  sapply(
-    ys,
-    function(y) y^2
-  )
-) - n * y_mean^2
-
+# Least squares estimators/estimates
+S_xx <- sum(sapply(xs, power_two)) - n * x_mean^2
+S_xy <- sum(mapply(`*`, xs, ys)) - n * x_mean * y_mean
+S_yy <- sum(sapply(ys, power_two)) - n * y_mean^2
 SS_R <- (S_xx * S_yy - S_xy^2) / S_xx
 
+# Regression parameters
 beta_est <- S_xy / S_xx
-
-variance_param <- SS_R / (n - 2)
-
 alpha_est <- y_mean - beta_est * x_mean
 
+# Variance parameter
+variance_param <- SS_R / (n - 2)
+
+# Coefficient of determination
 coeff_of_determination <- (S_yy - SS_R) / S_yy
 
+# Test statistic
 TS <- sqrt(
   ((n - 2) * S_xx) / SS_R
 ) * beta_est
 
+# Print results
 list(
   n = n,
   y_mean = y_mean,
